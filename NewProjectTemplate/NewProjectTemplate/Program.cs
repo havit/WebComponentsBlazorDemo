0b93d4f273e229ce9ceb43c9f9c606089d2f01b4
Robert Haken <haken@havit.cz>
2025-06-27 15:29:46 +0200

    Initial commit

---
+++ NewProjectTemplate/NewProjectTemplate/Program.cs
@@ -0,0 +1,42 @@
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+using NewProjectTemplate.Client.Pages;
+using NewProjectTemplate.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents()
+	.AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHxServices();
+builder.Services.AddHxMessenger();
+builder.Services.AddHxMessageBoxHost();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+}
+else
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(NewProjectTemplate.Client._Imports).Assembly);
+
+app.Run();
---
+++ SetSolutionName.ps1
@@ -0,0 +1,24 @@
+﻿param (
+        [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline=$true,
+        HelpMessage = "New solution name (e.g. NewProjectTemplate)")]
+		[string]$NewSolutionName
+)
+
+[string]$SolutionFolder = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path);
+
+Get-ChildItem -recurse $SolutionFolder -include *.cs,*.csproj,*.config,*.ps1,*.json,*.tsx,*.cshtml,*.props,*.razor,*.json,*.html,*.js,*.sln | where { $_ -is [System.IO.FileInfo] } | where { !$_.FullName.Contains("\packages\") } | where { !$_.FullName.Contains("\obj\") } | where { !$_.FullName.Contains("package.json") } | where { !$_.Name.Equals("_SetApplicationName.ps1") } |
+Foreach-Object {
+    Set-ItemProperty $_.FullName -name IsReadOnly -value $false
+    [string]$Content = [IO.File]::ReadAllText($_.FullName)
+    $Content = $Content.Replace('NewProjectTemplate', $NewSolutionName)
+    [IO.File]::WriteAllText($_.FullName, $Content, [System.Text.Encoding]::UTF8)
+}
+
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'Havit.Blazor.SimpleBlazorWebAppTemplate.sln')) -newName ($NewSolutionName + '.sln')
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'NewProjectTemplate\NewProjectTemplate\NewProjectTemplate.csproj')) -newName ($NewSolutionName + '.csproj')
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'NewProjectTemplate\NewProjectTemplate.Client\NewProjectTemplate.Client.csproj')) -newName ($NewSolutionName + '.Client.csproj')
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'NewProjectTemplate\NewProjectTemplate.Client')) -newName ($NewSolutionName + '.Client')
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'NewProjectTemplate\NewProjectTemplate')) -newName ($NewSolutionName)
+Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, 'NewProjectTemplate')) -newName ($NewSolutionName)
